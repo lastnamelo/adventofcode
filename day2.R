ed0902b09a9d22eddc0953cd1682de59bec71c6e
lastnamelo <lastnamelo@me.com>
2022-12-14 10:07:01 -0500

    Update day2.R

--- day2.R
+++ day2.R
@@ -89,3 +89,49 @@ Part2Ans <-sum(StrategyB)
 #Answer
 paste("Part 1:", Part1Ans)
 paste("Part 2:", Part2Ans)
+
+#MORE ELEGANT/EFFICIENT SOLUTION REF: ARDOMINE FOR SWITCH FUNCTION
+
+PuzzleInput <- read.csv(file = "PuzzleInput2.csv", header = FALSE)
+
+StrategyA <- as.matrix(PuzzleInput)
+
+for (i in 1:nrow(StrategyA)) {
+  StrategyA[i] <- 
+    switch (StrategyA[i],
+            'A X' = 1 + 3, 
+            'A Y' = 2 + 6, 
+            'A Z' = 3 + 0, 
+            'B X' = 1 + 0, 
+            'B Y' = 2 + 3, 
+            'B Z' = 3 + 6,
+            'C X' = 1 + 6, 
+            'C Y' = 2 + 0, 
+            'C Z' = 3 + 3
+    )
+}
+
+Part1Ans <- sum(as.numeric(StrategyA)) 
+
+#Part 2
+StrategyB <- as.matrix(PuzzleInput)
+
+for (i in 1:nrow(StrategyB)) {
+  StrategyB[i] <- 
+    switch (StrategyB[i],
+    'A X' = 3 + 0, 
+    'A Y' = 1 + 3, 
+    'A Z' = 2 + 6, 
+    'B X' = 1 + 0, 
+    'B Y' = 2 + 3, 
+    'B Z' = 3 + 6,
+    'C X' = 2 + 0, 
+    'C Y' = 3 + 3, 
+    'C Z' = 1 + 6
+    )
+}
+
+Part2Ans <- sum(as.numeric(StrategyB))  
+
+paste("Part 1:", Part1Ans)
+paste("Part 2:", Part2Ans)
